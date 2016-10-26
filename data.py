535bfef781f7892310ed0ca25cc40113b2381387
Zsolt Zombori <zombori@renyi.hu>
2016-10-26 12:57:55 +0200

    bugfix so that data.py work for tensorflow

--- data.py
+++ data.py
@@ -2,6 +2,7 @@ import numpy as np
 from keras.datasets import mnist, cifar10
 from keras.preprocessing.image import ImageDataGenerator
 from keras.utils import np_utils
+from keras import backend as K
 
 def load_data(dataset):
     if dataset == "cifar10":
@@ -11,8 +12,15 @@ def load_data(dataset):
         (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
         nb_classes = 10
         # ImageDataGenerator requires a color axis, but is okay with it being grayscale.
-        X_train = np.expand_dims(X_train, axis=1)
-        X_test = np.expand_dims(X_test, axis=1)
+        dim_ordering = K.image_dim_ordering()
+        if dim_ordering == 'th':
+            colorAxis = 1
+        elif dim_ordering == 'tf':
+            colorAxis = 3
+        else:
+            raise Exception('Invalid dim_ordering: ' + dim_ordering)
+        X_train = np.expand_dims(X_train, axis=colorAxis)
+        X_test = np.expand_dims(X_test, axis=colorAxis)
 
     X_train = X_train.astype('float32')
     X_test = X_test.astype('float32')
